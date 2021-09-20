261c624b51846d5f1283982d1b6b7e4c1b43fc68
Glauber Vilane Ferreira Marques <gvfmarques@hotmail.com>
2021-09-19 21:15:09 -0500

    Revert "Revert "Add project files.""
    This reverts commit 0f0e2034a813729b2157d6eae6c7cdd2ba57df06.

---
+++ ApiCatalogos/InputModel/JogoInputModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.InputModel
+{
+    public class JogoInputModel
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "O nome do jogo deve conter entre 3 e 100 caracteres")]
+        public string Nome { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "O nome da produtora deve conter entre 3 e 100 caracteres")]
+        public string Produtora { get; set; }
+
+        [Required]
+        [Range(1, 100, ErrorMessage = "O preco deve ter no minimo 1 real e no maximo 1000 reais")]
+        public double Preco { get; set; }
+    }
+}
